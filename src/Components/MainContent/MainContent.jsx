5a866149fec33e72e9a5b39eb2cf7b597674f830
Mohamed Yahia <m.yahya199290@gmail.com>
2024-09-12 11:59:54 +0300

    modified:   src/Components/MainContent/MainContent.js

--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -17,9 +17,25 @@ const MainContent = ({
 
     return (
         <div className="main-content">
-            <div className="container" style={{transform: `translateX(-${Number(MapOpen) * 100}vw)`}}>
-                {isLoading ? <Loading />: <PlaceDetails places={places} type={type} placeClicked={placeClicked}/>} 
-                <Map places={places} setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} SetPlaceClicked={SetPlaceClicked}/> 
+            <div 
+                className="container" 
+                style={{transform: `translateX(-${Number(MapOpen) * 100}vw)`}}
+                >
+                {
+                    isLoading ? <Loading />
+                    : <PlaceDetails 
+                        places={places} 
+                        type={type} 
+                        placeClicked={placeClicked}
+                      />
+                } 
+                <Map 
+                    places={places} 
+                    setCoordinates={setCoordinates} 
+                    setBounds={setBounds} 
+                    coordinates={coordinates} 
+                    SetPlaceClicked={SetPlaceClicked}
+                    /> 
             </div>
             
         </div>
